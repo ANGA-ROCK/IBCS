0e95f9140c681c7d2c55d34aec075341832973eb
RODO-ROCK <56351879+RODO-ROCK@users.noreply.github.com>
2019-10-15 16:09:12 +0200

    IBCS_To_PBI

--- Custom_Visual/src/visual.ts
+++ Custom_Visual/src/visual.ts
@@ -1,43 +1,19 @@
-/*
-*  Power BI Visual CLI
-*
-*  Copyright (c) Microsoft Corporation
-*  All rights reserved.
-*  MIT License
-*
-*  Permission is hereby granted, free of charge, to any person obtaining a copy
-*  of this software and associated documentation files (the ""Software""), to deal
-*  in the Software without restriction, including without limitation the rights
-*  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-*  copies of the Software, and to permit persons to whom the Software is
-*  furnished to do so, subject to the following conditions:
-*
-*  The above copyright notice and this permission notice shall be included in
-*  all copies or substantial portions of the Software.
-*
-*  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-*  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-*  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-*  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-*  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-*  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-*  THE SOFTWARE.
-*/
-"use strict";
-
 import "core-js/stable";
 import "./../style/visual.less";
 import powerbi from "powerbi-visuals-api";
 import VisualConstructorOptions = powerbi.extensibility.visual.VisualConstructorOptions;
 import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions;
 import IVisual = powerbi.extensibility.visual.IVisual;
+import DataViewObjects = powerbi.DataViewObjects;
 import EnumerateVisualObjectInstancesOptions = powerbi.EnumerateVisualObjectInstancesOptions;
 import VisualObjectInstance = powerbi.VisualObjectInstance;
 import DataView = powerbi.DataView;
 import IVisualHost = powerbi.extensibility.visual.IVisualHost;
 import VisualObjectInstanceEnumerationObject = powerbi.VisualObjectInstanceEnumerationObject;
 import { VisualSettings } from "./settings";
-
+// import ISelectionId = powerbi.visuals.ISelectionId;
+// import ISelectionIdBuilder = powerbi.visuals.ISelectionIdBuilder;
+// import ISelectionManager = powerbi.extensibility.ISelectionManager
 import * as d3 from "d3";
 type Selection<T extends d3.BaseType> = d3.Selection<T, any, any, any>;
 
@@ -59,57 +35,52 @@ export class Visual implements IVisual {
     private host: IVisualHost;
     private svg: Selection<SVGElement>;
     private barGroup: Selection<SVGElement>;
+    private axisGroup: Selection<SVGElement>;
     private visualSettings: VisualSettings;
+    private viewModel: ViewModel;
+    
+    private xPadding: number = 1/3;
+    private margin = {
+        outer: 20,
+        inner: 20,
+        fontSize:10,
+        fontPadding:4,
+        sideLabel:40,
+        chartOverlap:1/9,
+        patternSize:4,
+        blok:9.3,
+        tri: 5,
+        line: 4,
+        xPadding: 1/3
+    }
+
+    private color = {
+        black:"#000000",
+        darkGray:"#404040",
+        gray:"#939393",
+        lightGray: "#F5F5F5",
+        white:"#FFFFFF",
+        green:"#7ACA00",
+        red:"#FF0000"
+    }
+
+    private setting = {
+
+    }
 
     constructor(options: VisualConstructorOptions) {
         this.host = options.host;
         this.svg = d3.select(options.element)
-            .append("svg")
-            .classed("IBCS", true);
-        this.barGroup = this.svg.append("g")
-            .classed("bar-group", true);
-
-        this.target = options.element;
-        this.updateCount = 0;
-        if (typeof document !== "undefined") {
-            const new_p: HTMLElement = document.createElement("p");
-            new_p.appendChild(document.createTextNode("Update count:"));
-            const new_em: HTMLElement = document.createElement("em");
-            this.textNode = document.createTextNode(this.updateCount.toString());
-            new_em.appendChild(this.textNode);
-            new_p.appendChild(new_em);
-            this.target.appendChild(new_p);
-        }
+            .append("svg");
+        this.barGroup = this.svg.append("g").attr("class","actual");
+        this.axisGroup = this.svg.append("g").attr("class","axis");
     }
 
     public update(options: VisualUpdateOptions) {
-        let sample: DataPoint[] = [
-            {
-                category: "Apples",
-                value: 10
-            },
-            {
-                category: "Bananas",
-                value: 20
-            },
-            {
-                category: "Cherries",
-                value: 30
-            },
-            {
-                category: "Dates",
-                value: 40
-            },
-            {
-                category: "Elderberries",
-                value: 50
-            }
-        ];
 
-        let viewModel: ViewModel = {
-            dataPoints: sample,
-            maxValue: d3.max(sample, x => x.value)
-        };
+        this.updateSettings(options);
+
+        this.viewModel = this.getViewModel(options);
 
         let width = options.viewport.width;
         let height = options.viewport.height;
@@ -117,54 +88,76 @@ export class Visual implements IVisual {
         this.svg.attr("width", width);
         this.svg.attr("height", height);
 
-        let yScale = d3.scaleLinear()
-            .domain([0,viewModel.maxValue])
-            .range([height,0])
+        let y = d3.scaleLinear()
+            .domain([0,this.viewModel.maxValue])
+            .range([height - 50 ,0]);
 
-        let xscale = d3.scaleBand()
-            .domain(viewModel.dataPoints.map(function(d){return d.category}))
+        let x = d3.scaleBand()
+            .domain(this.viewModel.dataPoints.map(function(d){return d.category; }))
             .range([0, width])
-            .paddingInner(0.5)
-        
-        var xAxis = d3.axisBottom(xscale)
+            .paddingInner(this.margin.xPadding);
 
-        this.svg.append("g")
-            .attr("class","x axis")
-            // .style("font-size", ""+margin.fontSize*(1+1/3)+"px")
-            .style("font-size", "10pt")
-            .attr("transform","translate(4,"+(height-50)+")")
-            .call(xAxis)
+        let bars = this.barGroup.selectAll("rect.ac")
+            .data(this.viewModel.dataPoints)
+            .enter()
+            .append("rect")
+                .attr("class","ac")
+                .attr("transform","translate(0,-50)")
+                .attr("x", function(d) { return x(d.category); })
+                .attr("y", function(d) { return y(d.value); })
+                .attr("width", x.bandwidth())
+                .attr("height", function(d) { return height - y(d.value); })
+                .attr("style","fill:#404040");
 
-        let bars = this.barGroup
-            .selectAll(".bar")
-            .data(viewModel.dataPoints)
-            .enter().append("rect")
-            .classed("bar", true)
-            .attr("x", function(d) { return xscale(d.category); })
-            .attr("y", function(d) { return yScale(d.value); })
-            .attr("width", xscale.bandwidth())
-            .attr("height", function(d) { return height - yScale(d.value); });
+        let axis = this.axisGroup.selectAll("text.label")
+            .data(this.viewModel.dataPoints)
+            .enter()
+            .append("text")
+                .text(function (d) {return d.category}) 
+                .attr("transform","translate(0,"+ (height) +")")
+                .attr("class","label")
+                .attr("x", function (d) { return x(d.category) + x.bandwidth() / 2; })
+                .attr("y", 0)
+                .attr("style","text-align: center; text-anchor: middle; font-family: Arial ;font-size: 10pt;")
 
         bars.exit()
             .remove();
+    }
 
-        this.settings = Visual.parseSettings(options && options.dataViews && options.dataViews[0]);
-        console.log('Visual update', options);
-        if (typeof this.textNode !== "undefined") {
-            this.textNode.textContent = (this.updateCount++).toString();
+    private updateSettings(options: VisualUpdateOptions) {
+
+    }
+
+    private getViewModel(options: VisualUpdateOptions): ViewModel {
+
+        let dv = options.dataViews;
+        let viewModel: ViewModel = {
+            dataPoints: [],
+            maxValue: 0,
+        };
+        
+        if (!dv
+            || !dv[0]
+            || !dv[0].categorical
+            || !dv[0].categorical.categories
+            || !dv[0].categorical.categories[0].source
+            || !dv[0].categorical.values
+            || !dv[0].metadata)
+            return viewModel;
+        
+        let view = dv[0].categorical;
+        let categories = view.categories[0];
+        let values = view.values[0];
+        let highlights = values.highlights;
+
+        for (let i = 0, len = Math.max(categories.values.length, values.values.length); i < len; i++) {
+            viewModel.dataPoints.push({
+                category: <string>categories.values[i],
+                value: <number>values.values[i],
+            });
         }
-    }
 
-    private static parseSettings(dataView: DataView): VisualSettings {
-        return VisualSettings.parse(dataView) as VisualSettings;
-    }
-
-    /**
-     * This function gets called for each of the objects defined in the capabilities files and allows you to select which of the
-     * objects and properties you want to expose to the users in the property pane.
-     *
-     */
-    public enumerateObjectInstances(options: EnumerateVisualObjectInstancesOptions): VisualObjectInstance[] | VisualObjectInstanceEnumerationObject {
-        return VisualSettings.enumerateObjectInstances(this.settings || VisualSettings.getDefault(), options);
+        viewModel.maxValue = d3.max(viewModel.dataPoints, function(d){return d.value; });
+        return viewModel;
     }
 }
